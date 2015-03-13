f1ac9237d25e493c8c2961f6b05c7b596ee236d4
Syra <dasddas>
2015-03-13 12:50:32 +0000

    Data tokens broken down and stored
    Data tokens are analysed to see whether they're a ped or a car. if its a
    car the data of their entrance and exit are stored in an array

--- src/InputThread.java
+++ src/InputThread.java
@@ -1,8 +1,14 @@
 import java.net.*; 
 import java.io.*; 
 import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.lang.String;
+
+
 public class InputThread implements Runnable {
     
+    private int[] carsEntered = {0,0,0,0};
+    private int[] carsExited = {0,0,0,0};
     Socket echoSocket; 
 
     InputThread(Socket socket)
@@ -20,11 +26,20 @@ public class InputThread implements Runnable {
 
 
             while (true) {
-                System.out.println("echo: " + in.readLine());
-                StringTokenizer st = new StringTokenizer(in.readLine(),":;");
+                //System.out.println("echo: " + in.readLine());
+                StringTokenizer st = new StringTokenizer(in.readLine(),";");
                 while (st.hasMoreTokens()) 
                 {
-                    System.out.println("this: "+st.nextToken());
+                    String temp = st.nextToken();
+                    if(temp.length() == 1)
+                    {
+                        System.out.println("Ett");
+                    }
+                    else
+                    {
+                        breakdownToken(temp);  
+                    }
+                    System.out.println("this: "+temp);
                 }
                 
             }		
@@ -36,6 +51,46 @@ public class InputThread implements Runnable {
             System.err.println();  
             System.exit(1); 
         }
+    }
+    public void breakdownToken(String data){
+        StringTokenizer breakdown = new StringTokenizer(data,":");
+        boolean ioCheck = true;
+        while(breakdown.hasMoreTokens())
+        {
+            String current = breakdown.nextToken();
+            if(ioCheck == true)
+            {
+                carsEntered = enterData(carsEntered,current);
+            }
+            else
+            {
+                carsExited = enterData(carsExited,current);
+            }
+            System.out.println(current);
+            ioCheck = !ioCheck;
+        }
+        System.out.println(java.util.Arrays.toString(carsEntered));
+        System.out.println(java.util.Arrays.toString(carsExited));
+    }
+    public int[] enterData(int[] array, String token)
+    {
+        switch(token){
+            case "1":
+                array[0]++;
+                break;
+            case "2":
+                array[1]++;
+                break;
+            case "3":
+                array[2]++;
+                break;
+            case "4":
+                array[3]++;
+                break;
+            default:
+                break;
+        }
+        return array;
     }
 }
 
